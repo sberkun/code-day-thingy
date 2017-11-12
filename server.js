ee6ce9f094815b8d5e9e15d95561016307498ca3
rsambouncer <rsambouncer@gmail.com>
2017-11-12 08:49:17 -0800

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const server = express()
 const wss = new SocketServer({ server });
 const peoples = {};
 var buildingCosts = [15,97,1057,11528,124878,1344835,19211921,316996684,4899039651,72044700750,960596010000,13448344140000,163301321700000,2017251621000000,24975496259999996];
+                            console.log(buildingCosts[7]);
 var sendOBJ = {
   score1:0,score2:0,cps1:0,cps2:0,
   ppl:peoples,
@@ -37,6 +38,8 @@ setInterval(function(){
       
       if(peoples[aa]['u']===0){}else{
         var cooost = buildingCosts[Math.abs(peoples[aa]['u'])-1];
+        console.log(Math.abs(peoples[aa]['u'])-1);
+        console.log(cooost);
         if(peoples[aa]['u']<0&&sendOBJ.score1>=cooost){
           sendOBJ.score1-=cooost;
           sendOBJ.cps1+=cooost/100;
