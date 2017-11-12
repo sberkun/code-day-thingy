bcfdbf17d8caf6d8ba10ff669c313b043f930ef9
rsambouncer <rsambouncer@gmail.com>
2017-11-12 09:10:33 -0800

    Update server.js

--- server.js
+++ server.js
@@ -15,13 +15,15 @@ const server = express()
 const wss = new SocketServer({ server });
 const peoples = {};
 var buildingCosts = [15,97,1057,11528,124878,1344835,19211921,316996684,4899039651,72044700750,960596010000,13448344140000,163301321700000,2017251621000000,24975496259999996];
-                            console.log(buildingCosts[7]);
 var sendOBJ = {
   score1:0,score2:0,cps1:0,cps2:0,
   ppl:peoples,
   u1:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   u2:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 };
+var actualscore1 = 0;
+var actualscore2 = 0;
+var timey = new Date().getTime();
 wss.on('connection', (ws) => {
   ws.id = Math.random();
   peoples[ws.id] = {a:0,b:0,x:0,y:0};
@@ -33,25 +35,29 @@ wss.on('connection', (ws) => {
 
 setInterval(function(){    
     for(var aa in peoples){
-      sendOBJ.score1+=peoples[aa]['a'];
-      sendOBJ.score2+=peoples[aa]['b'];
+      actualscore1+=peoples[aa]['a'];
+      actualscore2+=peoples[aa]['b'];
       
       if(peoples[aa]['u']===0){}else{
         var cooost = buildingCosts[Math.abs(peoples[aa]['u'])-1];
-        console.log(Math.abs(peoples[aa]['u'])-1);
-        console.log(cooost);
-        if(peoples[aa]['u']<0&&sendOBJ.score1>=cooost){
-          sendOBJ.score1-=cooost;
+        if(peoples[aa]['u']<0&&actualscore1>=cooost){
+          actualscore1-=cooost;
           sendOBJ.cps1+=cooost/100;
         }
-        if(peoples[aa]['u']>0&&sendOBJ.score2>=cooost){
-          sendOBJ.score2-=cooost;
+        if(peoples[aa]['u']>0&&actualscore2>=cooost){
+          actualscore2-=cooost;
           sendOBJ.cps2+=cooost/100;
         }  
       }
       
     }
     
+    actualscore1+= (new Date().getTime()-timey)*sendOBJ.cps1*0.001;
+    actualscore2+= (new Date().getTime()-timey)*sendOBJ.cps2*0.001;
+    timey = new Date().getTime();
+    sendOBJ.score1 = Math.floor(actualscore1);
+    sendOBJ.score2 = Math.floor(actualscore2);
+  
     wss.clients.forEach((client) => {
       client.send(JSON.stringify(sendOBJ));
     });
